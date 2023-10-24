5914fe057379d2e642c25bb34248195362da641f
Mishywayu <michellewayua2@gmail.com>
2023-10-24 07:45:45 +0300

    Learning and Unlearning

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "@/node_modules/next/image";
+
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({
+  children
+}) => {
+  return (
+);
+}
+
+export default Container;
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from '../Container'
+
+export default function Navbar() {
+  return (
+    <div className='fixed w-full z-10 bg-white shadow-sm'>
+        <div className='py-4 border-b-[1px]'>
+            <Container>
+              <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+                <Logo />
+              </div>
+            </Container>
+        </div>
+    </div>
+  )
+}
